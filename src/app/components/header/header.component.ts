0869d24c56c1904dafbcb08dc2e33f9a651fc78d
orjon <github@orjon.com>
2024-12-05 13:21:59 +0000

    Angular Task Manager - initial commit.

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent implements OnInit {
+  ngOnInit() {
+    // Initialization logic here
+  }
+}
